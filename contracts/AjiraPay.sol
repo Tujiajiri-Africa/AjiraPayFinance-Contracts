f690074ec00eb19927a9edb657b6ff55a2c04eed
Dickens Odera <dickens@laevitas.ch>
2022-08-03 17:34:16 +0300

    feat: normal ERC20 functions

--- contracts/AjiraPay.sol
+++ contracts/AjiraPay.sol
@@ -44,10 +44,8 @@ contract AjiraPay is Ownable,AccessControl,ReentrancyGuard, IERC20{
     event NewDevTreasuryFee(address indexed caller, uint indexed newDevTreasuryFee, uint timestamp);
     event NewMarketingTreasuryFee(address indexed caller, uint indexed newMarketingTresuryFee, uint indexed timestamp);
 
-    constructor(address _router, address payable _devTreasury, address payable _marketingTreasury){
+    constructor(address _router){
         require(_router != address(0),"Ajira Pay: Zero Address detected");
-        require(_devTreasury != address(0),"Ajira Pay: Zero Address detected");
-        require(_marketingTreasury != address(0),"Ajira Pay: Zero Address detected");
 
         _name = 'Ajira Pay';
         _symbol = 'AJP';
@@ -55,8 +53,6 @@ contract AjiraPay is Ownable,AccessControl,ReentrancyGuard, IERC20{
         _totalSupply = 200_000_000 * 10 ** _decimals;
         balances[msg.sender] = _totalSupply;
 
-        devTreasury = _devTreasury;
-        marketingTreasury = _marketingTreasury;
         emit Transfer(address(0), msg.sender, _totalSupply);
     }
 
@@ -108,20 +104,34 @@ contract AjiraPay is Ownable,AccessControl,ReentrancyGuard, IERC20{
         return _decimals;
     }
 
-    function totalSupply() external view override returns(uint){
+    function totalSupply() public view override returns(uint){
         return _totalSupply;
     }
 
-    function balanceOf(address _account) external view override returns (uint){
+    function balanceOf(address _account) public view override returns (uint){
         return balances[_account];
     }
 
-    function allowance(address _owner, address _spender) external view returns (uint){
+    function allowance(address _owner, address _spender) public view returns (uint){
         return allowances[_owner][_spender];
     }
 
-    function transfer(address _to, uint256 _amount) external returns (bool){
 
+    function transfer(address _to, uint256 _amount) public virtual override returns (bool) {
+        address owner = _msgSender();
+        _transfer(owner, _to, _amount);
+        return true;
+    }
+
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _amount
+    ) public virtual override returns (bool) {
+        address spender = _msgSender();
+        _spendAllowance(_from, spender, _amount);
+        _transfer(_from, _to, _amount);
+        return true;
     }
 
     function approve(address _spender, uint256 _amount) public virtual override returns (bool) {
@@ -130,7 +140,46 @@ contract AjiraPay is Ownable,AccessControl,ReentrancyGuard, IERC20{
         return true;
     }
 
+    function increaseAllowance(address _spender, uint256 _addedValue) public virtual returns (bool) {
+        address owner = _msgSender();
+        _approve(owner, _spender, allowance(owner, _spender) + _addedValue);
+        return true;
+    }
+
+    function decreaseAllowance(address _spender, uint256 _subtractedValue) public virtual returns (bool) {
+        address owner = _msgSender();
+        uint256 currentAllowance = allowance(owner, _spender);
+        require(currentAllowance >= _subtractedValue, "Ajira Pay: decreased allowance below zero");
+        unchecked {
+            _approve(owner, _spender, currentAllowance.sub(_subtractedValue));
+        }
+
+        return true;
+    }
+
     //Internal Functions 
+    function _transfer(
+        address from,
+        address to,
+        uint256 amount
+    ) internal virtual {
+        require(from != address(0), "Ajira Pay: transfer from the zero address");
+        require(to != address(0), "Ajira Pay: transfer to the zero address");
+
+        _beforeTokenTransfer(from, to, amount);
+
+        uint256 fromBalance = balances[from];
+        require(fromBalance >= amount, "Ajira Pay: transfer amount exceeds balance");
+        unchecked {
+            balances[from] = fromBalance.sub(amount);
+        }
+        balances[to] = balances[to].add(amount);
+
+        emit Transfer(from, to, amount);
+
+        _afterTokenTransfer(from, to, amount);
+    }
+
     function _approve(
         address _owner,
         address _spender,
@@ -142,4 +191,88 @@ contract AjiraPay is Ownable,AccessControl,ReentrancyGuard, IERC20{
         allowances[_owner][_spender] = _amount;
         emit Approval(_owner, _spender, _amount);
     }
+
+    function _mint(address _account, uint256 _amount) internal virtual {
+        require(_account != address(0), "Ajira Pay: mint to the zero address");
+
+        _beforeTokenTransfer(address(0), _account, _amount);
+
+        _totalSupply = _totalSupply.add(_amount);
+        balances[_account] = balances[_account].add(_amount);
+        emit Transfer(address(0), _account, _amount);
+
+        _afterTokenTransfer(address(0), _account, _amount);
+    }
+
+    function _burn(address _account, uint256 _amount) internal virtual {
+        require(_account != address(0), "Ajira Pay: burn from the zero address");
+
+        _beforeTokenTransfer(_account, address(0), _amount);
+
+        uint256 accountBalance = balances[_account];
+        require(accountBalance >= _amount, "Ajira Pay: burn amount exceeds balance");
+        unchecked {
+            balances[_account] = accountBalance.sub(_amount);
+        }
+        _totalSupply = _totalSupply.sub(_amount);
+
+        emit Transfer(_account, address(0), _amount);
+
+        _afterTokenTransfer(_account, address(0), _amount);
+    }
+
+    function _spendAllowance(
+        address _owner,
+        address _spender,
+        uint256 _amount
+    ) internal virtual {
+        uint256 currentAllowance = allowance(_owner, _spender);
+        if (currentAllowance != type(uint256).max) {
+            require(currentAllowance >= _amount, "Ajira Pay: insufficient allowance");
+            unchecked {
+                _approve(_owner, _spender, currentAllowance.sub(_amount));
+            }
+        }
+    }
+
+
+    /**
+     * @dev Hook that is called before any transfer of tokens. This includes
+     * minting and burning.
+     *
+     * Calling conditions:
+     *
+     * - when `from` and `to` are both non-zero, `amount` of ``from``'s tokens
+     * will be transferred to `to`.
+     * - when `from` is zero, `amount` tokens will be minted for `to`.
+     * - when `to` is zero, `amount` of ``from``'s tokens will be burned.
+     * - `from` and `to` are never both zero.
+     *
+     * To learn more about hooks, head to xref:ROOT:extending-contracts.adoc#using-hooks[Using Hooks].
+     */
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 amount
+    ) internal virtual {}
+
+    /**
+     * @dev Hook that is called after any transfer of tokens. This includes
+     * minting and burning.
+     *
+     * Calling conditions:
+     *
+     * - when `from` and `to` are both non-zero, `amount` of ``from``'s tokens
+     * has been transferred to `to`.
+     * - when `from` is zero, `amount` tokens have been minted for `to`.
+     * - when `to` is zero, `amount` of ``from``'s tokens have been burned.
+     * - `from` and `to` are never both zero.
+     *
+     * To learn more about hooks, head to xref:ROOT:extending-contracts.adoc#using-hooks[Using Hooks].
+     */
+    function _afterTokenTransfer(
+        address from,
+        address to,
+        uint256 amount
+    ) internal virtual {}
 }
