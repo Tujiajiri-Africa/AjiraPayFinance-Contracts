77bff9c304a693aef3b8f1a0e195e7754c15e1fe
dickensodera <dickens@laevitas.ch>
2022-09-29 13:26:39 +0300

    update: code refactor

--- contracts/AjiraPayAirdropDistributor.sol
+++ contracts/AjiraPayAirdropDistributor.sol
@@ -5,14 +5,17 @@ import '@openzeppelin/contracts/access/AccessControl.sol';
 import '@openzeppelin/contracts/access/Ownable.sol';
 import '@openzeppelin/contracts/security/ReentrancyGuard.sol';
 import '@openzeppelin/contracts/utils/math/SafeMath.sol';
+import '@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol';
+
 
 contract AjiraPayAirdropDistributor is Ownable, AccessControl, ReentrancyGuard{
     using SafeMath for uint256;
+    using SafeERC20 for IERC20;
 
     IERC20 public rewardToken;
 
     bool public isAirdropActive = false;
-    bool canClaim = false;
+    bool public isClaimOpen = false;
 
     bytes32 constant public MANAGER_ROLE = keccak256('MANAGER_ROLE');
 
@@ -27,9 +30,11 @@ contract AjiraPayAirdropDistributor is Ownable, AccessControl, ReentrancyGuard{
     event AirdropDeActivated(address indexed caller, IERC20 indexed token, uint indexed timestamp);
     event RewardTokenSet(address indexed caller, IERC20 indexed token, uint timestamp);
     event NewWinner(address indexed caller, address indexed winner, uint indexed amount, uint timestamp);
-    event NewAirdropPayout(address indexed caller, address indexed beneciary, uint indexed amount, uint timestamp);
+    event ClaimFor(address indexed caller, address indexed beneciary, uint indexed amount, uint timestamp);
     event Claim(address indexed beneficiary, uint indexed amount, uint timestamp);
-    event UserRewardUpdated(address indexed beneficiary, uint indexed prevRewardAmount, uint indexed newRewardAmount, uint timestamp);
+    event UserRewardUpdated(address indexed caller, address indexed beneficiary, uint prevRewardAmount, uint indexed newRewardAmount, uint timestamp);
+    event ClaimsOpened(address indexed caller, uint indexed timestamp);
+    event ClaimsClosed(address indexed caller, uint indexed timestamp);
 
     modifier isActive(){
         require(isAirdropActive == true,"Airdrop not active");
@@ -56,6 +61,16 @@ contract AjiraPayAirdropDistributor is Ownable, AccessControl, ReentrancyGuard{
         _;
     }
 
+    modifier claimOpen(){
+        require(isClaimOpen == true,"Claim Not Active");
+        _;
+    }
+
+    modifier claimClosed(){
+        require(isClaimOpen == false,"Claim Not Active");
+        _;
+    }
+
     constructor(IERC20 _token, uint _minRewardCap, uint _maxRewardCap, uint _tokenDecimals){
         require(_tokenDecimals > 0 && _tokenDecimals <= 18,"Invalid Decimals Number");
         _grantRole(DEFAULT_ADMIN_ROLE, _msgSender());
@@ -78,49 +93,61 @@ contract AjiraPayAirdropDistributor is Ownable, AccessControl, ReentrancyGuard{
 
     function addWinner(address _winner, uint _amount) public nonZeroAddress(_winner) onlyRole(MANAGER_ROLE){
         require(_amount > 0,"Amount is zero");
-        require(_amount < rewardToken.totalSupply() && _amount <= maxRewardCapPerUser,"Cannot send total supply");
+        require(_amount < rewardToken.balanceOf(address(this)) && _amount <= maxRewardCapPerUser,"Cap Reached");
         if(isExistingWinner[_winner] == false){ isExistingWinner[_winner] = true;}
         userRewards[_winner] = userRewards[_winner].add(_amount);
         emit NewWinner(_msgSender(), _winner, _amount, block.timestamp);
     }
 
-    function updateWinnerReward(address _winner, uint _newRewardAmount) public nonZeroAddress(_winner) isExistingWinnerAccount(_winner) onlyRole(MANAGER_ROLE)  {
-        uint256 rewardBefore = userRewards[_winner];
+    function updateWinnerReward(address _winner, uint _newRewardAmount) public nonZeroAddress(_winner) isExistingWinnerAccount(_winner) onlyRole(MANAGER_ROLE) nonReentrant {
         require(_newRewardAmount > 0,"Amount is zero");
-        require(_newRewardAmount < rewardToken.totalSupply() && _newRewardAmount <= maxRewardCapPerUser,"Cannot send total supply");
-        require(rewardBefore.add(_newRewardAmount) < rewardToken.totalSupply(),"");
-        
+        uint256 rewardBefore = userRewards[_winner];
+        uint256 totalRewards = rewardBefore.add(_newRewardAmount);
+        require(_newRewardAmount < rewardToken.balanceOf(address(this)) && _newRewardAmount <= maxRewardCapPerUser,"Cap Reached");
+        require(totalRewards <= rewardToken.balanceOf(address(this)) && totalRewards <= maxRewardCapPerUser,"Cap Reached");
         userRewards[_winner] = rewardBefore.add(_newRewardAmount);
         uint256 rewardAfter = userRewards[_winner];
-        emit UserRewardUpdated(_winner, rewardBefore, rewardAfter, block.timestamp);
+        if(hasClaimedRewards[_winner] == true && rewardAfter >0){
+            hasClaimedRewards[_winner] = false;
+        }
+        emit UserRewardUpdated(_msgSender(), _winner, rewardBefore, rewardAfter, block.timestamp);
     }
 
-    function distributeReward(address _beneficiary) public nonZeroAddress(_beneficiary) hasNotClaimedReward(_beneficiary) onlyRole(MANAGER_ROLE) isActive{
-        require(isExistingWinner[_beneficiary] = true,"Not a beneficiary");
-        uint256 rewardAmount = userRewards[_beneficiary];
-        require(rewardToken.transfer(_beneficiary,rewardAmount),"Failed to send reward");
-        userRewards[_beneficiary] = 0;
-        hasClaimedRewards[_beneficiary] = true;
-        emit NewAirdropPayout(_msgSender(),_beneficiary, rewardAmount, block.timestamp);
+    function claimAirdrop() public{
+        (uint256 _claimedRewardAmount) = _performClaim(_msgSender());
+        emit Claim(_msgSender(), _claimedRewardAmount, block.timestamp);
     }
 
-    function setRewardToken(address _token) public nonZeroAddress(_token) onlyRole(MANAGER_ROLE){
+    function claimAirdropFor(address _beneficiary) public onlyRole(MANAGER_ROLE) {
+        (uint256 _claimedRewardAmount) = _performClaim(_beneficiary);
+        emit ClaimFor(_msgSender(),_beneficiary, _claimedRewardAmount, block.timestamp);
+    }
+
+    function setRewardToken(address _token) public nonZeroAddress(_token) onlyRole(MANAGER_ROLE) isNotActive{
         rewardToken = IERC20(_token);
         emit RewardTokenSet(_msgSender(), rewardToken, block.timestamp);
     }
 
-    function claimAirdrop() public hasNotClaimedReward(msg.sender){
-        uint256 rewardAmount = userRewards[msg.sender];
-        require(rewardToken.transfer(msg.sender, rewardAmount),"Failed To Claim");
-        emit Claim(msg.sender, rewardAmount, block.timestamp);
+    function activateClaims() public onlyRole(MANAGER_ROLE) isActive claimClosed{
+        isClaimOpen = true;
+        emit ClaimsOpened(_msgSender(), block.timestamp);
+    }
+
+    function deActivateClaims() public onlyRole(MANAGER_ROLE) isActive claimOpen{
+        isClaimOpen = false;
+        emit ClaimsClosed(_msgSender(), block.timestamp);
     }
 
     function getAirdropTotalSupply() public view returns(uint256){
         return rewardToken.balanceOf(address(this));
     }
 
-    function activateClaims() public onlyRole(MANAGER_ROLE){
-        canClaim = true;
-        //TODO add more logic here including validations and events
+    //Internal functions
+    function _performClaim(address _beneficiary) private nonZeroAddress(_beneficiary) isExistingWinnerAccount(_beneficiary) hasNotClaimedReward(_beneficiary) isActive claimOpen nonReentrant returns(uint256){
+        uint256 rewardAmount = userRewards[_beneficiary];
+        require(rewardToken.transfer(_beneficiary,rewardAmount),"Failed to send reward");
+        userRewards[_beneficiary] = 0;
+        hasClaimedRewards[_beneficiary] = true;
+        return rewardAmount;
     }
 }
