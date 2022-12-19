52f7b3f1aedcc1c620ebb8afb99672a3fb8bf58c
dickensodera <dickens@laevitas.ch>
2022-12-19 16:07:02 +0300

    update: Update functions

--- contracts/AjiraPayStableCoinPresale.sol
+++ contracts/AjiraPayStableCoinPresale.sol
@@ -15,6 +15,8 @@ contract AjiraPayStableCoinPresale is Ownable, AccessControl,ReentrancyGuard{
     IERC20 public immutable USDC;
     IERC20 public AjiraPayFinanceToken;
 
+    AggregatorV3Interface internal priceFeed;
+
     constructor() {
         DAI = IERC20(0x1AF3F329e8BE154074D8769D1FFa4eE058B1DBc3);
         BUSD = IERC20(0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56);
@@ -23,6 +25,28 @@ contract AjiraPayStableCoinPresale is Ownable, AccessControl,ReentrancyGuard{
         AjiraPayFinanceToken = IERC20(0xC55b03dC07EC7Bb8B891100E927E982540f0d181);
     }
 
-    
+    function stableCoinPurchase(address _stableCoin, uint _amount) public nonReentrant{
+        (address _priceFeedAddress) = _getPriceFeedFromAddress(_stableCoin);
+        
+    }
+
+    function _getPriceFeedFromAddress(address _stableCoin) private returns(address){
+        if(_stableCoin == address(DAI)){
+            return address(DAI);
+        }else if(_stableCoin == address(BUSD)){
+            return address(BUSD);
+        }else if(_stableCoin == address(USDT)){
+            return address(USDT);
+        }else if(_stableCoin == address(USDC)){
+            return address(USDC);
+        }
+    }
+
+    function _getLatestBNBPriceInUSD() private view returns(uint256, uint256){
+        (, int256 price, , , ) = priceFeed.latestRoundData();
+        uint256 decimals = priceFeed.decimals();
+        return (uint256(price), decimals);
+    }
+
 
 }
